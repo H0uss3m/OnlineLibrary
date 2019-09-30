4f8b3e862542f39f6c3b885df29b52c921faacaa
H0uss3m <hossem.benmustapha@gmail.com>
2019-09-30 11:38:14 +0100

    protect routes with auth guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,9 +18,11 @@ import { Routes, RouterModule } from '@angular/router';
 const appRoutes: Routes = [
   { path: 'auth/signup', component: SignupComponent },
   { path: 'auth/signin', component: SigninComponent },
-  { path: 'books', component: BookListComponent },
-  { path: 'books/new', component: BookFormComponent },
-  { path: 'books/view/:id', component: SingleBookComponent }
+  { path: 'books', canActivate: [AuthGuardService], component: BookListComponent },
+  { path: 'books/new', canActivate: [AuthGuardService], component: BookFormComponent },
+  { path: 'books/view/:id', canActivate: [AuthGuardService], component: SingleBookComponent },
+  { path: '', redirectTo: 'books', pathMatch: 'full' },
+  { path: '**', redirectTo: 'books' }
 ]
 
 @NgModule({
--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
-
+import { CanActivate, Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
 
-  constructor() { }
+  constructor(private router: Router) { }
+  canActivate(): Observable<boolean> | Promise<boolean> | boolean {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.auth().onAuthStateChanged(
+          (user) => {
+            if(user){
+              resolve(true) 
+              
+            } else{
+
+              this.router.navigate(['/auth', 'signin']);
+              resolve(false)
+            } 
+          }
+        )
+      }
+    )
+  }
 }
