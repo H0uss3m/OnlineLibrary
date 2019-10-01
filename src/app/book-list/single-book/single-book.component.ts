562630914f2e3d896ce5ee267ab48647c9b66ca1
H0uss3m <hossem.benmustapha@gmail.com>
2019-10-01 08:53:55 +0100

    create book item

--- src/app/book-list/single-book/single-book.component.ts
+++ src/app/book-list/single-book/single-book.component.ts
@@ -1,4 +1,7 @@
+import { BooksService } from './../../services/books.service';
+import { Book } from './../../models/Book.model';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-single-book',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./single-book.component.scss']
 })
 export class SingleBookComponent implements OnInit {
-
-  constructor() { }
+  book: Book;
+  constructor(private route: ActivatedRoute,
+    private booksService: BooksService,
+    private router: Router) { }
 
   ngOnInit() {
+    this.book = new Book('', '');
+    const id = this.route.snapshot.params['id'];
+    this.booksService.getSingleBook(+id).then(
+      (book: Book) => {
+        this.book = book
+      }
+    );
+  }
+  onBack() {
+    this.router.navigate(['/books']);
   }
 
 }
